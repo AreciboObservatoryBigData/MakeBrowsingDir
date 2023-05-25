cdd6e95bc88dec123b9d8731cd8c093d425cc5ac
eborgesAO <eloim.borges.ao@gmail.com>
2023-05-25 16:31:37 -0400

    Finished multiprocessing implementation yet to test the complete of 60K files

--- mongo_export.py
+++ mongo_export.py
@@ -1,7 +1,6 @@
 from pymongo import MongoClient
 from connect_database import connect_to_mongodb_listing
 from connect_database import connect_to_mongodb_relation
-import glob
 import os
 from bson.objectid import ObjectId
 import time
@@ -21,13 +20,12 @@ def main():
     relation = connect_to_mongodb_relation()
 
 
-
     # Query documents where filetype is "d"
-    # query = {'filetype': 'd'}
-    test_query = {
-        'filepath': '/stornext/ranch_103/ranch/projects/Arecibo-Observatory/Legacy/Sciences/Atmospheric-Sciences/T1193'
-        }
-    query = test_query
+    query = {'filetype': 'd'}
+    # test_query = {
+    #     'filepath': '/stornext/ranch_103/ranch/projects/Arecibo-Observatory/Legacy/Sciences/Atmospheric-Sciences/T1193'
+    #     }
+    # query = test_query
     results = collection.find(query)
 
     if reset_dir == True:
@@ -37,6 +35,10 @@ def main():
     command = "mkdir " + folders_path
     os.system(command)
 
+
+    start_time = time.time()
+    arguments = []
+    i = 1
     for result in results:
         
         line=result['filepath']
@@ -46,58 +48,71 @@ def main():
         if not os.path.exists(new_output_dir_path):
             command = "mkdir -p '" + new_output_dir_path + "'"
             os.system(command)
-        
 
-        start_time = time.time()
-        total_size=0
-        # Add recursive size
-        # Get all file Ids in dst_file_dir where dir_ID == result["_id"]
-        id=result['_id']
-        recursive = relation.find({'dir_ID':ObjectId(id)})
-        i=0
-        print(f"Proj _Id: {id}")
-        total_size = 0
-        arguments = []
-        for_loop_time = time.time()
-        for x in recursive:
-            file=x['file_ID']
-            # Example
-            # size_start_time = time.time()
-            # size = getSize(file)  
-            # print("Size took :" + str(time.time()-size_start_time))
-            # total_size = total_size + size 
-            arguments.append(file)
-            i=i+1
-        print(time.time() - for_loop_time)
-        print(f"Count:  {i}")
-        # submit to multiprocessing
-        pool = mp.Pool(100)
-        multi_time = time.time()
-        print("Tasks submitted to multiprocessing")
-        results = pool.map(getSize, arguments)
-        print(f"Multiprocessing finished {time.time()-multi_time}")
-        pool.close()
-        pool.join()
-        total_size =  0
-        for x in results:
-            total_size = total_size + x
+        arguments.append(result["_id"])
+        if i % 10000 == 0:
+            print(i)
+        i += 1
+    #implement multiprocess
+    pool = mp.Pool(48)
+    print("Submitting tasks")
+    results = pool.map(multiprocessLookup, arguments)
+    pool.close()
+    pool.join()
+
+    # for argument in arguments[100:]:
+    #     print(argument)
+    #     size = multiprocessLookup(argument)
+
+
     
-        print(f"Total size: {total_size}")
+    for result in results:
+        print(result)
+        total_time = time.time() - start_time
+    print(total_time)
         
-        elapsed_time = time.time() - start_time
-        print("Elapsed time:", elapsed_time, "seconds\n\n")
-
-# function should recieve the file_ID and return the filesize of that file
-def getSize(id):
-    # start_time = time.time()
-    selected = collection.find_one({'_id':ObjectId(id)}, {"filesize": 1})
-    size=selected['filesize']
-    # print(time.time() - start_time)
-    return size
 
 
+def multiprocessLookup(object_ID):
+    # relation = connect_to_mongodb_relation()
+    aggregation = [
+            {
+                '$match': {
+                    'dir_ID': object_ID
+                }
+            }, {
+                '$lookup': {
+                    'from': 'dst_listing', 
+                    'localField': 'file_ID', 
+                    'foreignField': '_id', 
+                    'as': 'result'
+                }
+            }, {
+                '$project': {
+                    'filesize': {
+                        '$arrayElemAt': [
+                            '$result.filesize', 0
+                        ]
+                    }
+                }
+            }, {
+                '$group': {
+                    '_id': None, 
+                    'totalSize': {
+                        '$sum': '$filesize'
+                    }
+                }
+            }
+        ]
+    result = relation.aggregate(aggregation)
+    result = [item for item in result]
+    if len(result)==0:
+        return_value = 0
+    else:
+        return_value = result[0]["totalSize"]
 
 
-
+    return return_value
+    
 
 main()
